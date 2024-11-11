0b2877cb1b695cffb4e641c2014b4f6b4e0e48ef
SahilSharma2710 <s90sahil90@gmail.com>
2024-11-11 11:12:38 +0530

    more changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
 }
